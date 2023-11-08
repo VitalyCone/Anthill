82bb3ffa3af483e6ed064a49a54238963a884ff9
lidefo <lidefo753@gmail.com>
2023-11-08 22:13:16 +0400

    fixed documentation & renamed variables

--- Ant.py
+++ Ant.py
@@ -35,35 +35,50 @@ class Ant:
         self.ants = self.get_ants(self.scene)  # Вообще все муравьи
         self.spiders = self.get_spiders(self.scene)  # Вообще все пауки
 
-    def get_spiders(self, scene):  # Фукнция возвращает всех пауков из сцены
+    def get_spiders(self, scene):
+        """
+        Фукнция возвращает всех пауков из сцены
+        """
         spiders = []
-        for spider in scene:
-            if spider.name == 'Spider':
-                spiders.append(spider)
+        for agent in scene:
+            if agent.name == 'Spider':
+                spiders.append(agent)
         return spiders
 
-    def get_ants(self, scene):  # Возвращает всех муравьев из сцены
+    def get_ants(self, scene):
+        """
+        Возвращает всех муравьев из сцены
+        """
         ants = []
-        for ant in scene:
-            if ant.name == 'Ant':
-                ants.append(ant)
+        for agent in scene:
+            if agent.name == 'Ant':
+                ants.append(agent)
         return ants
 
-    def get_apples(self, scene):  # Возвращает все яблоки из сцены
+    def get_apples(self, scene):
+        """
+        Возвращает все яблоки из сцены
+        """
         apples = []
-        for apple in scene:
-            if apple.name == 'Apple':
-                apples.append(apple)
+        for agent in scene:
+            if agent.name == 'Apple':
+                apples.append(agent)
         return apples
 
-    def get_anthill(self, scene):  # Возвращает муравейники сцены(задел на будущее)
+    def get_anthill(self, scene):
+        """
+        Возвращает муравейники сцены(задел на будущее)
+        """
         anthill_1 = None
-        for anthill in scene:
-            if anthill.name == 'Anthill':
-                anthill_1 = anthill
+        for agent in scene:
+            if agent.name == 'Anthill':
+                anthill_1 = agent
         return anthill_1
 
-    def body(self):  # Построение тела на карте
+    def body(self):
+        """
+        Построение тела на карте
+        """
         if self.charachter == 0:  # Трус - чуть поменьше
             s = random.randint(4, 5)
             return pygame.Rect(self.geo[0], self.geo[1], s,
@@ -79,11 +94,14 @@ class Ant:
         except:
             print("Убить не получилось!")
 
-    def self_determination(self, state, ants, spiders, apples):  # Самоопределение.
-        # Самоопределение ответственно только за анализ ситуации и возвращение состояния муравья
-        # Самое первое - если есть опасность, надо либо от нее убежать,
-        # либо ее победить. Уже после идут всё остальное. Разумеется, это пока что, и
-        # в планах разработать систему удовлетворенность и противовесов.
+    def self_determination(self, state, ants, spiders, apples):
+        """
+        Самоопределение.
+        Самоопределение ответственно только за анализ ситуации и возвращение состояния муравья
+        Самое первое - если есть опасность, надо либо от нее убежать,
+        либо ее победить. Уже после идут всё остальное. Разумеется, это пока что, и
+        в планах разработать систему удовлетворенность и противовесов.
+        """
 
         if self.anthill.exit == True:  # Если конец игры, то возвращается значение,
             if spiders != []:  # если таки встретился паучара, ему не сдобровать!
@@ -94,8 +112,7 @@ class Ant:
         if state[0] == 4:  # Если параметр равен 4, то убегать от паука(см. комментрий к переменной state)
             if len(ants) < 5 and spiders != []:  # Если окружающих мурававьев < 5 и список окружающих пауков не пуст, то
                 return [4, spiders[0]]  # Возвращается значение "бегство!"
-            if state[
-                1] not in spiders:  # если преследующего паука больше нет в радиусе, то отменить бегство и приняться
+            if state[1] not in spiders:  # если преследующего паука больше нет в радиусе, то отменить бегство и начать
                 return [0, 0]  # искать яблоки
 
         if state[0] == 2:  # Если параметр равен 2, то идет проверка, а существует ли еще такой паук в радиусе
@@ -112,7 +129,7 @@ class Ant:
                 consensus = self.send_message_to_radius(0, ants, spiders[0])
                 if consensus == True:
                     self.state = [2, spiders[0]]
-                elif consensus == False:
+                else:
                     self.state = [4, spiders[0]]
             elif apples != []:  # Если пауков нет -> если есть яблоки
                 if state[1] not in apples or self.get_distance(
@@ -124,32 +141,40 @@ class Ant:
                 return [0, 0]  # если ничего из этого не прошло, то просто поиск яблока
 
         if state[0] == 0:  # Если состояние - простой поиск яблока
-            if len(ants) < 5 and spiders != []:  # Если нет муравьев  и есть пауки - бегство
+            if len(ants) < 5 and spiders != []:  # Если нет муравьев и есть пауки - бегство
                 return [4, spiders[0]]
             elif len(ants) > 5 and spiders != []:  # Если муравьев много - призыв к бойне!
                 consensus = self.send_message_to_radius(0, ants, spiders[0])
                 if consensus == True:
                     self.state = [2, spiders[0]]
-                elif consensus == False:
+                else:
                     self.state = [4, spiders[0]]
             elif apples != []:  # ну авось если яблочки есть, то извольте-с его тащить
-                if self.get_distance(apples[0]) <= 5: return [1, apples[0]]
+                if self.get_distance(apples[0]) <= 5:
+                    return [1, apples[0]]
         return state
 
-    def live_by_self_determination(self, geo, state, ants, spiders, apples):  # Жить по самоопределению
+    def live_by_self_determination(self, geo, state, ants, spiders, apples):
+        """
+        Жить по самоопределению
+        """
         if geo[0] >= 990 or geo[0] <= 10 or geo[1] >= 990 or geo[1] <= 10:
 
-            if geo[0] >= 990: self.u = math.pi + self.u
-            if geo[0] <= 10: self.u = math.pi + self.u
-            if geo[1] >= 990: self.usin = math.pi + self.usin
-            if geo[1] <= 10: self.usin = math.pi + self.usin
+            if geo[0] >= 990:
+                self.u = math.pi + self.u
+            elif geo[0] <= 10:
+                self.u = math.pi + self.u
+            if geo[1] >= 990:
+                self.usin = math.pi + self.usin
+            elif geo[1] <= 10:
+                self.usin = math.pi + self.usin
         else:
-            if state[0] == 0:
+            if state[0] == 0:  # в поиске яблока
                 if spiders != []:
                     consensus = self.send_message_to_radius(0, ants, spiders[0])
                     if consensus == True:
                         self.state = [2, spiders[0]]
-                    elif consensus == False:
+                    else:
                         self.state = [4, spiders[0]]
                 elif apples != []:
                     try:
@@ -161,7 +186,7 @@ class Ant:
                     self.u = random.uniform(self.u - self.gamma, self.u + self.gamma)
                     self.usin = random.uniform(self.usin - self.gamma, self.usin + self.gamma)
 
-            elif state[0] == 1:
+            elif state[0] == 1:  # нашёл яблоко и тащит его
                 if spiders != [] and ants != 0:
                     consensus = self.send_message_to_radius(0, ants, spiders[0])
                     if consensus == True:
@@ -179,14 +204,18 @@ class Ant:
                     self.scene = state[1].move(self.scene)
                 elif self in state[1].travelset and state[1] not in apples:
                     state[1].travelset.remove(self)
-            elif state[0] == 2:
+
+            elif state[0] == 2:  # атака паука
                 self.u = math.acos((state[1].geo[0] - geo[0]) / self.get_distance(state[1]))
                 self.usin = math.asin((state[1].geo[1] - geo[1]) / self.get_distance(state[1]))
-            elif state[0] == 3:
+
+            elif state[0] == 3:  # конец игры (возвращение в муравейник)
                 self.u = math.acos((self.anthill.geo[0] - geo[0]) / self.get_distance(self.anthill))
                 self.usin = math.asin((self.anthill.geo[1] - geo[1]) / self.get_distance(self.anthill))
-                if self.get_distance(self.anthill) <= 4: self.die(self)
-            elif state[0] == 4:
+                if self.get_distance(self.anthill) <= 4:
+                    self.die(self)
+
+            elif state[0] == 4:  # бегство от угрозы
                 self.u = math.pi - math.acos((self.anthill.geo[0] - geo[0]) / self.get_distance(self.anthill))
                 self.usin = math.pi - math.asin((self.anthill.geo[1] - geo[1]) / self.get_distance(self.anthill))
 
@@ -211,18 +240,24 @@ class Ant:
         self.live_by_self_determination(self.geo, self.state, self.ants, self.spiders, self.apples)
 
         self.energy -= 0.001
-        if self.energy <=0:
+        if self.energy <= 0:
             self.die(self)
 
         for agent in self.scene:
-            full_scene.append(
-                agent)  # после окончания хода, паук передает в сцену изменившиеся данные и возвращает ее диспетчеру вместе с ходом(простите, без элементарного диспетчера не получался нормальный паук)
+            full_scene.append(agent)  # после окончания хода, паук передает в сцену изменившиеся данные и возвращает ее
+            # диспетчеру вместе с ходом(простите, без элементарного диспетчера не получался нормальный паук)
         return full_scene
 
-    def get_distance(self, obj):  # возвращает информацию о расстоянии до обьекта при помощи любимой теоремы Пифагора
+    def get_distance(self, obj):
+        """
+        Возвращает информацию о расстоянии до объекта при помощи любимой теоремы Пифагора
+        """
         return math.sqrt((self.geo[0] - obj.geo[0]) ** 2 + (self.geo[1] - obj.geo[1]) ** 2)
 
-    def accept_message(self, param, obj):  # Функция принятия сообщения. 0 - принятия просьбы о помощи
+    def accept_message(self, param, obj):
+        """
+        Функция принятия сообщения. 0 - принятия просьбы о помощи
+        """
         if param == 0:
             if self.charachter == 0:
                 if self.state[0] != 2 and self.get_distance(obj) <= self.r * 3:
@@ -235,8 +270,10 @@ class Ant:
                 elif obj in self.spiders:
                     self.state = [4, obj]
 
-    def send_message_to_radius(self, param, ants,
-                               obj=0):  # Функция разослать сообщение всем в радиусе. param = 0 - помогите убить паука
+    def send_message_to_radius(self, param, ants, obj=0):
+        """
+        Функция разослать сообщение всем в радиусе. param = 0 - помогите убить паука
+        """
         if param == 0:
             if len(ants) > 3:
                 for ant in ants:
@@ -245,7 +282,10 @@ class Ant:
             else:
                 return False
 
-    def get_scene(self, scene):  # возвращает обьекты из сцены, в радиусе обзора паука
+    def get_scene(self, scene):
+        """
+        Возвращает обьекты из сцены, в радиусе обзора паука
+        """
         scene1 = []
         for obj in scene:
             if obj.name == "Ant":
@@ -260,6 +300,10 @@ class Ant:
 
         return scene1
 
-    def run(self):  # метод, который перемещает муравья в нужном направлении, после рассчета хода(сделан отдельно, т. к.  в будующем можно будет отделить планировщик от рендеринга)
+    def run(self):
+        """
+        Метод, который перемещает муравья в нужном направлении,
+        после расчета хода(сделан отдельно, т. к. в будущем можно будет отделить планировщик от рендеринга)
+        """
         self.geo[0] += self.speed * math.cos(self.u)
         self.geo[1] += self.speed * math.sin(self.usin)
